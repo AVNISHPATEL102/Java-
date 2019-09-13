3a40f459a991d006d69e02b63f03dde67f2baf7a
AVNISHPATEL102 <48069185+AVNISHPATEL102@users.noreply.github.com>
2019-09-13 14:42:04 -0400

    Update q13.java

--- Chapter2/src/q13.java
+++ Chapter2/src/q13.java
@@ -1,29 +1,32 @@
 import java.util.Scanner;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.File;
 public class q13 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner fileReader = null;
+		Scanner fileReader=null;
 		String line;
 		try
 		{
-			fileReader = new Scanner(new FileInputStream("Sample.rtf"));
+			fileReader = new Scanner(new File("/Users/mr.a/Desktop/eclipse/Chapter2/src/Height.txt"));
 		}
 		catch(FileNotFoundException e)
 		{
 			System.out.println("File not found");
 			System.exit(0);
 		}
-		
+		for(int i=0;i<3;i++) {
 		line = fileReader.nextLine();
-		System.out.println("The text file line: Sample.txt");
+		System.out.println("The text file line: Height.txt");
 		System.out.println(line);
-		int position =line.indexOf("hate");
-		String changedLine = line.substring(0,position)+"love"+line.substring(position+"hate".length());
-		System.out.println(changedLine);
-		
+		int a = fileReader.nextInt();
+		int b= fileReader.nextInt();
+		fileReader.nextLine();
+		int weight = ((((a-5)*12)+b)*5)+110;
+		System.out.println(weight);
+		}
 	}
 
 }
